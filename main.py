41853b72decc7fc50a4169a24c9b6ed33c33fc83
metameg <metameg8@gmail.com>
2023-02-01 18:49:16 -0700

    singleton added

--- main.py
+++ main.py
@@ -2,16 +2,13 @@
 
 # Import and initialize the pygame library
 import pygame
+from GridLayout import GridLayout
+
 
 
 pygame.init()
+grid = GridLayout()
 
-
-game_display = pygame.display.set_mode((640, 640))
-blackfish = pygame.image.load('art/blackfish.png')
-brick = pygame.image.load('art/tiles/brick.png')
-crate = pygame.image.load('art/tiles/crate.png')
-grass = pygame.image.load(('art/tiles/grass.png'))
 
 blackfishX = 0
 blackfishY = 0
@@ -26,32 +23,6 @@ tileSet = [   [2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
               [2, 0, 0, 0, 0, 0, 0, 0, 0, 2]]
 
-##### Changes only made to this function. #################
-# You can change each block to whatever image you want in the tileSet matrix above.              
-
-def redrawGameWindow():
-
-    startX, startY = 0, 0
-    for row in tileSet:
-        for square in row:
-
-            if square == 0:
-                game_display.blit(grass, (startX, startY))
-
-            elif square == 1:
-                game_display.blit(crate, (startX, startY))
-            
-            elif square == 2:
-                game_display.blit(brick, (startX, startY))
-
-            startX += 64
-
-        startY += 64
-        startX = 0
-
-    game_display.blit(blackfish, (blackfishX, blackfishY))
-
-# Set up the drawing window
 
 
 
@@ -69,10 +40,9 @@ while run:
 
     keys = pygame.key.get_pressed()
 
-    game_display.blit(crate, (0, 0))
-    game_display.fill((0, 0, 0))
     
-    redrawGameWindow()
+    
+    grid.redrawGameWindow(grid.top_left)
     if keys[pygame.K_a]:
         blackfishX -= velocity
     elif keys[pygame.K_d]:
